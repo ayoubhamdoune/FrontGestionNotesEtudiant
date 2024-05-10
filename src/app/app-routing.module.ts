b0fe8c8b45357048ea43e8893c44443e16998b17
Oussama-Azouggarh <oussamasabir36@gmail.com>
2024-05-10 19:04:30 +0100

    tset push

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MatieresComponent } from './matieres/matieres.component';
+import { NewMatiereComponent } from './new-matiere/new-matiere.component';
 
 const routes: Routes = [
   {
     path:"matieres",component: MatieresComponent
+  },
+  {
+    path:"new-matiere",component:NewMatiereComponent
+  },
+  {
+
   }
 ];
 
--- src/app/matieres/matieres.component.ts
+++ src/app/matieres/matieres.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-matieres',
   templateUrl: './matieres.component.html',
   styleUrls: ['./matieres.component.css']
 })
-export class MatieresComponent {
+export class MatieresComponent implements OnInit{
+  
+  constructor() {}
+  ngOnInit() {
+    
+  }
 
 }
