a2fb976eaa75c48de0ae48c5061918faebe829a1
Oussama-Azouggarh <oussamasabir36@gmail.com>
2024-05-10 23:20:30 +0100

    addmatrtab

--- src/app/matieres/matieres.component.ts
+++ src/app/matieres/matieres.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
 
 @Component({
   selector: 'app-matieres',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./matieres.component.css']
 })
 export class MatieresComponent implements OnInit{
-  
-  constructor() {}
+  public matieres:any;
+  constructor(private httpClient:HttpClient) {
+  }
   ngOnInit() {
-    
+
   }
 
+  onGetMatieres() {
+
+    this.httpClient.get("http://localhost:8080/matieres")
+      .subscribe(data=>{
+        this.matieres=data;
+      },error => {
+        console.log(error);
+      })
+
+  }
 }
