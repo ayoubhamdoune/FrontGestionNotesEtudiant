ead9bd54f746ef393fb70fbebee055eca4c69c8b
hamdouneayoub <ayoubhamdoune.pro@gmail.com>
2024-05-10 19:03:57 +0100

    okay

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
+import { MatieresComponent } from './matieres/matieres.component';
 import { EtudiantsComponent } from './etudiants/etudiants.component';
 
 const routes: Routes = [ 
   { path: 'etudiants', component: EtudiantsComponent },
 
-=======
-import { MatieresComponent } from './matieres/matieres.component';
 
-const routes: Routes = [
   {
     path:"matieres",component: MatieresComponent
   }
->>>>>>> d95f0718053407dfb405ca45b4d9b8b2b8c27026
+
 ];
 
 @NgModule({
