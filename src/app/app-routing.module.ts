5ece002c681e2cd2b529b582ef3681e5b9a5f353
hamdouneayoub <ayoubhamdoune.pro@gmail.com>
2024-05-10 18:34:29 +0100

    etudiants

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EtudiantsComponent } from './etudiants/etudiants.component';
 
-const routes: Routes = [];
+const routes: Routes = [ 
+  { path: 'etudiants', component: EtudiantsComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
